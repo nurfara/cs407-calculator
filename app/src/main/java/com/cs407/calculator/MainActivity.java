a11df310620465d1566b9ec854a0816fabce4c5e
nurfara <32235232+nurfara@users.noreply.github.com>
2023-09-21 14:46:02 -0500

    Update

--- app/src/main/java/com/cs407/calculator/MainActivity.java
+++ app/src/main/java/com/cs407/calculator/MainActivity.java
@@ -2,7 +2,11 @@ package com.cs407.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void clickFunction(View view) {
+        EditText firstNum = findViewById(R.id.firstNum);
+        EditText secondNum = findViewById(R.id.secondNum);
+        String firstNumStr = firstNum.getText().toString();
+        String secondNumStr = secondNum.getText().toString();
+        if (!firstNumStr.isEmpty() && !secondNumStr.isEmpty()) {
+            int num1 = Integer.parseInt(firstNumStr);
+            int num2 = Integer.parseInt(secondNumStr);
+
+            switch(view.getId()) {
+             //   case R.id.plusButton: //FIXME
+              //      break;
+            }
+        }
+    }
+
+
 }
